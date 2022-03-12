d1f3b850207a90ec7fc898320de18f8cf1e8a3f2
TollyH <ptolemyhill@sky.com>
2022-03-12 00:27:10 +0000

    Created README

---
+++ README.md
@@ -0,0 +1,24 @@
+# PyGame Maze
+
+A simple, grid-based maze game written in Python with PyGame.
+
+## Controls
+
+- `Arrow keys` or `WASD` - Move around
+- `[` and `]` - Previous/Next Level Respectively
+- `R` - Reset level
+
+## Cheat Controls
+
+- `Click` - Teleport
+- `Space` - Reveal all paths and shortest path to exit/keys
+- `CTRL` + `Space` - Auto complete level
+- `Mouse Wheel` - Increase/Decrease auto completion delay
+
+## Instructions
+
+- White tiles can be moved on, black tiles cannot.
+- You (the blue tile) need to collect all of the keys (the gold tiles)
+before reaching the exit (the green tile).
+- When showing the solution, all possible paths are highlighted in lilac, with
+the shortest one being highlighted in purple.
--- __main__.py
+++ __main__.py
@@ -1,23 +1,8 @@
 """
 PyGame Maze - Tolly Hill 2022
 
-Controls:
-Arrow keys or WASD - Move around
-[ and ] - Previous/Next Level Respectively
-R - Reset level
-
-Cheat Controls:
-Point and Click - Teleport
-Space - Reveal all paths and shortest path to exit/keys
-CTRL + Space - Auto complete level
-Mouse Wheel - Increase/Decrease auto completion delay
-
-Instructions:
-White tiles can be moved on, black tiles cannot.
-You (the blue tile) need to collect all of the keys (the gold tiles)
-before reaching the exit (the green tile).
-When showing the solution, all possible paths are highlighted in lilac, with
-the shortest one being highlighted in purple.
+The main script for the game. Creates and draws to the game window, as well as
+receiving and interpreting player input and recording time and movement scores.
 """
 import sys
 
