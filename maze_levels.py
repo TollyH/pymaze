d2047871106ea92e8d13c471c10e463c190ae66f
TollyH <ptolemyhill@sky.com>
2022-03-13 19:44:32 +0000

    Added new level

--- maze_levels.py
+++ maze_levels.py
@@ -75,6 +75,39 @@ levels = [
             (14, 16), (18, 10), (2, 16), (10, 18), (8, 14), (0, 0), (6, 10),
             (8, 6), (14, 0), (16, 6), (12, 14)
         ]
+    ),
+    Level(
+        (20, 20),
+        [
+            [T, F, T, T, T, F, F, T, T, F, F, T, T, T, F, F, F, T, T, T],
+            [F, F, F, F, T, T, F, F, F, F, T, T, F, T, T, T, F, F, F, F],
+            [T, F, T, F, F, F, F, T, T, F, F, F, F, F, F, T, F, T, T, F],
+            [T, F, T, T, T, T, T, T, T, T, F, T, F, T, F, T, F, T, F, F],
+            [T, F, F, F, F, F, T, T, F, T, T, T, T, T, F, T, F, F, F, T],
+            [T, T, T, F, T, F, T, F, F, F, F, F, F, T, F, T, T, T, F, F],
+            [T, F, T, T, T, F, T, T, F, T, T, T, F, F, F, T, T, F, F, T],
+            [T, F, T, F, F, F, T, T, F, F, T, F, F, T, T, T, F, F, F, F],
+            [F, F, T, F, T, T, T, T, T, F, T, T, F, T, F, F, F, T, F, T],
+            [F, T, T, F, T, F, F, F, T, F, F, T, T, T, F, T, T, T, F, T],
+            [F, T, F, F, T, F, T, T, T, F, F, F, F, F, F, F, F, T, T, T],
+            [F, F, F, T, T, F, F, F, F, F, T, T, T, T, T, T, F, F, F, T],
+            [T, T, F, T, T, T, T, T, F, F, F, T, F, F, F, T, T, T, F, F],
+            [T, F, F, F, F, F, F, T, T, F, T, T, F, T, F, F, F, T, F, T],
+            [F, F, T, F, T, T, F, F, T, T, T, T, F, T, T, T, F, T, T, T],
+            [F, T, T, F, T, T, T, F, F, T, F, F, F, T, F, T, F, F, F, T],
+            [T, T, F, F, T, F, T, T, F, F, F, T, F, T, F, T, T, T, F, F],
+            [T, F, F, T, T, F, F, T, T, F, T, T, F, T, F, F, T, F, F, T],
+            [T, F, T, T, T, T, F, T, F, F, F, T, F, F, F, T, T, F, T, T],
+            [T, F, F, F, F, F, F, T, T, F, T, T, T, T, T, T, F, F, F, F]
+        ],
+        (9, 9), (19, 19),
+        [
+            (1, 0), (5, 0), (10, 0), (14, 0), (0, 1), (12, 1), (10, 3),
+            (12, 3), (8, 4), (3, 5), (7, 5), (19, 5), (1, 6), (11, 7), (19, 7),
+            (12, 8), (7, 9), (18, 9), (10, 12), (19, 12), (9, 13), (18, 13),
+            (0, 15), (14, 15), (5, 16), (19, 16), (15, 17), (8, 18), (10, 18),
+            (9, 19), (16, 19)
+        ]
     )
 ]
 
